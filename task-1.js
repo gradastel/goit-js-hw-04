57b2bd8cd3b6beddd3d13e13f4ec35b9ff785f89
Vladyslav Levchenko <gradastel@gmail.com>
2020-07-26 17:51:56 +0300

    changes

--- task-1.js
+++ task-1.js
@@ -6,24 +6,25 @@ const account = {
   discount: 0.1,
   orders: ["order-1", "order-2", "order-3"],
   changeDiscount(value) {
-    account.discount = value;
-    return this.value;
+    this.discount = value;
   },
+
   showOrders() {
     return this.orders;
   },
+
   addOrder(cost, order) {
-    account.balance -= cost;
-    account.orders.push(order);
-    return this.cost, order;
+    this.balance -= cost;
+    this.orders.push(order);
   },
 };
 
 account.changeDiscount(0.15);
 console.log(account.discount); // 0.15
 
-console.log(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+const orders = account.showOrders();
+console.log(orders); // ['order-1', 'order-2', 'order-3']
 
 account.addOrder(5000, "order-4");
 console.log(account.balance); // 19000
-console.log(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+console.log(account.orders); // ['order-1', 'order-2', 'order-3', 'order-4'];
